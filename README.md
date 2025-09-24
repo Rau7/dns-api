7a40717912c18a7bee50f3baf2492b130575f052
Alp Toker <tokeralp@gmail.com>
2025-09-24 20:02:41 +0300

    ControllerAdded

--- README.md
+++ README.md
@@ -7,3 +7,36 @@ Steps:
 2- Framework bundle and routing bundle added.
 
 3- Create a service for domains (we need to separate the logic from the controller). (isValidDomain, isDomainExists, getDnsRecords)
+
+4- Create a Controller for homepage and api endpoint for dns records.
+
+5- All set lets test this with postman
+
+    - http://localhost:8000/api/dns/thisdoesnotexist12345.com
+
+
+    result = {
+                "error": "Domain does not exist",
+                "domain": "thisdoesnotexist12345.com"
+            }
+
+    - http://localhost:8000/api/dns/google.com
+
+    result = {
+            "success": true,
+            "domain": "google.com",
+            "records": {
+                "A": [
+                    "142.250.187.174"
+                ],
+                "AAAA": [
+                    "2a00:1450:4017:80f::200e"
+                ],
+                "MX": [
+                    {
+                        "host": "smtp.google.com",
+                        "priority": 10
+                    }
+                ]
+            }
+        }
---
+++ src/Controller/DnsController.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+@Author : Alp Toker
+This is a Controller for do the actual work.
+*/
+
+namespace App\Controller;
+
+use App\Service\DnsService;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class DnsController extends AbstractController
+{
+
+    public function __construct(private DnsService $dnsSrv) {}
+
+    /*
+        For seeing the dns form page
+    */
+    #[Route('/', name: 'dns_home')]
+    public function index(): Response
+    {
+        return $this->render('dns/index.html.twig');
+    }
+
+    /*
+        For getting the dns records,
+        By using DnsService we can check the constraints
+    */
+    #[Route('/api/dns/{domain}', name: 'api_dnss', methods: ['GET'])]
+    public function getDnsRecords(string $domain): JsonResponse
+    {
+        //Check if it is a valid domain
+        if (!$this->dnsSrv->isValidDomain($domain)) {
+            return $this->json([
+                'error' => 'Invalid domain format',
+                'domain' => $domain
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
+        //Check if the domain exists
+        if (!$this->dnsSrv->isDomainExists($domain)) {
+            return $this->json([
+                'error' => 'Domain does not exist',
+                'domain' => $domain
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        //Getting the records but we should be aware that it may throw an exception
+        try {
+            $records = $this->dnsSrv->getDnsRecords($domain);
+            
+            return $this->json([
+                'success' => true,
+                'domain' => $domain,
+                'records' => $records
+            ]);
+            
+        } catch (\Exception $e) {
+
+            return $this->json([
+                'error' => 'Failed to fetch DNS records',
+                'domain' => $domain
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+
+
+
+}
